53b8c8d542c38e4fa4958c8ec0d5aa53eb213620
PZWJAY <pzwjay@mail.ustc.edu.cn>
2018-02-01 17:32:39 +0800

    add a README.txt

--- code/using_trend.py
+++ code/using_trend.py
@@ -3,3 +3,6 @@ import warnings
 warnings.filterwarnings('ignore')
 
 df_train = pd.read_csv('../data/[new] yancheng_train_20171226.csv')
+
+df_train_sub = df_train[(df_train['class_id'] == 103507)].groupby(['sale_date']).sale_quantity.sum().round()
+a = 0
